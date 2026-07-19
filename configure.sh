78c2a95befc7b6be33731a50113a8822cf7ea3e6
xxx <141565625+beXquiet@users.noreply.github.com>
2026-07-19 18:47:20 +0300

    Update VNC configuration and ngrok installation commands

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ sudo createhomedir -c -u vncuser > /dev/null
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+#VNC password
 echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
 #Start VNC/reset changes
@@ -26,8 +26,9 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
 #install ngrok
-brew cask install ngrok
+brew install --cask ngrok
 
 #configure ngrok and start it
-ngrok authtoken $3
-ngrok tcp 5900 &
+ngrok config add-authtoken $3
+nohup ngrok tcp 5900 > /tmp/ngrok.log 2>&1 &
+sleep 5
